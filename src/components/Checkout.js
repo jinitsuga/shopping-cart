03d8f9a926548672771b1c05545bd7b30e89012d
agu <iba.agustin@gmail.com>
2022-06-15 20:20:25 -0300

    mini form validation left for checkout form

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,6 +3,8 @@ import "./Checkout.css";
 export default function Checkout(props) {
   const [isFormShown, setIsFormShown] = React.useState(false);
   const [buyerName, setBuyerName] = React.useState("");
+  const [buyerLocation, setBuyerLocation] = React.useState("");
+  const [isOrderSent, setIsOrderSent] = React.useState(false);
   const cart = props.cart;
   const total = Math.floor(
     cart.reduce(function (previous, current) {
@@ -10,15 +12,29 @@ export default function Checkout(props) {
     }, 0)
   );
   // need to send total price and resetCart from props
-  function payTotal() {}
+  function keepBrowsing() {}
+  function placeOrder() {
+    props.resetCart();
+    console.log(
+      "Thank you " +
+        buyerName +
+        " for your purchase! Your order is heading to " +
+        buyerLocation
+    );
+  }
   function showPaymentForm() {
     setIsFormShown(true);
   }
+  function saveBuyerLocation(e) {
+    setBuyerLocation(e.target.value);
+  }
   function saveBuyerName(e) {
-    setBuyerName(e.event.target.value);
+    setBuyerName(e.target.value);
     console.log(buyerName);
   }
-
+  function avoidSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <section className="checkout">
       <div className="pay-btns">
@@ -27,25 +43,46 @@ export default function Checkout(props) {
           style={!isFormShown ? { display: "block" } : { display: "none" }}
           onClick={showPaymentForm}
         >
-          Pay $ {total} in laughs :D
+          Pay $ {total} in laughs :D{<br></br>} ←
         </button>
         <button
           type="button"
           style={!isFormShown ? { display: "block" } : { display: "none" }}
           onClick={showPaymentForm}
         >
-          Pay ${total} in smiles :)
+          Pay ${total} in smiles :) {<br></br>} →
         </button>
       </div>
       <form
+        onSubmit={avoidSubmit}
         className="payment-info"
         style={!isFormShown ? { display: "none" } : { display: "block" }}
       >
-        <input
-          className="payment-name"
-          placeholder="Name"
-          onChange={saveBuyerName}
-        ></input>
+        <div className="payment-details">
+          <span>Enter your name:</span>
+          <input
+            className="payment-name"
+            placeholder="Name"
+            onChange={saveBuyerName}
+          ></input>
+          <span>Location:</span>
+          <input
+            className="payment-location"
+            placeholder="Location/city"
+            onChange={saveBuyerLocation}
+          ></input>
+          <button
+            className="place-order-btn"
+            type="button"
+            onClick={placeOrder}
+          >
+            Place order
+          </button>
+          <p
+            className="thank-text"
+            style={isOrderSent ? { display: "block" } : { display: "none" }}
+          ></p>
+        </div>
       </form>
     </section>
   );
